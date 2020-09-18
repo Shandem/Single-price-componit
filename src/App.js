eda4172fa70cfb87ab3880487c0021af2728ab60
Joshuah Gutierrez <gutierrezjoshuah@gmail.com>
2020-09-18 16:19:29 -0700

    first project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="card">
+      <div className="top">
+        <h1>Join our community</h1>
+        <h3>30-day, hassle-free money back guarantee</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Gain access to out full library of tutorials along with 
+          expert code reviews. <br/> Perfect for any developers who are serious
+          about honing their skills.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </div>
+
+        <div className="bottom">
+          <div className='left'>
+            <h3>Monthly Subscription</h3>
+            <h1> 
+              $29 <span>per month</span>
+            </h1>
+            <p>
+              Full access for less thean $1 a day
+            </p>
+            <button>Sign up</button>
+          </div>
+
+          <div className='right'> 
+            <h3>Why us</h3> 
+            <ul>
+              <li>Tutorials by industry experts</li>
+              <li>Peer & expert code review</li>
+              <li>Coding exercises</li>
+              <li>Access to our GitHub repos</li>
+              <li>Community forum</li>
+              <li>Flashcard Decks</li>
+              <li>New videos every week</li>
+            </ul> 
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
